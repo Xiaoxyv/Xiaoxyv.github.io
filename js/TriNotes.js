893cfe88440daa3904f5ae037a04487968b9ce62
Xiaoxyv <76772272+Xiaoxyv@users.noreply.github.com>
2024-01-02 00:22:24 +0800

    Update TriNotes.js

--- js/TriNotes.js
+++ js/TriNotes.js
@@ -8,7 +8,7 @@
 function setSeed(){
     var userSeed = document.getElementById("seed").value;
     docCookies.setItem("usrSeed",userSeed,Infinity,"/","xiaoxyv.github.io",false);
-    alert(docCookies.getItem("userSeed"));
+    alert(docCookies.getItem("usrSeed"));
 }
 
 
